e7e8c73e27277372ff1cf07b3cf0b9f9ab426099
maytham alherz <gmaytham@gmail.com>
2022-03-08 04:54:45 +0300

    fake os 0.0.1

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # sary-task
+
+url: 
+
+### Infra
+Folder is for infrastructure, using pulumi as the Iac.
+
+### kube
+Folder kubernetes deployments, manifests and helm charts.
+
+### fake-stackoverflow
+Folder where the Golang implementation of the fake stackoverflow.
+
+### .github/workflows
+For CD github actions is used. still missing some features.
--- fake-stackoverflow/models/model.go
+++ fake-stackoverflow/models/model.go
@@ -13,34 +13,34 @@ type Users struct {
 
 type Answers struct {
 	gorm.Model
-	Body     string    `json:"body"`
-	User     Users     `gorm:"foreignKey:ID" json:"user"`
-	Question Questions `gorm:"foreignKey:ID" json:"questions"`
+	Body     string    `json:"body" binding:"required"`
+	User     Users     `gorm:"foreignKey:ID" json:"user" binding:"required"`
+	Question Questions `gorm:"foreignKey:ID" json:"questions" binding:"required"`
 }
 
 type Questions struct {
 	gorm.Model
-	Tital string `json:"tital"`
+	Tital string `json:"tital" binding:"required"`
 	Body  string `json:"body"`
 	Tags  []Tags `gorm:"foreignKey:Name" json:"tags"`
-	User  Users  `gorm:"foreignKey:ID" json:"users"`
+	User  Users  `gorm:"foreignKey:ID" json:"users" binding:"required"`
 }
 
 type CommentsOnQuestion struct {
 	gorm.Model
-	Body     string    `json:"body"`
-	User     Users     `gorm:"foreignKey:ID" json:"user"`
-	Question Questions `gorm:"foreignKey:ID" json:"question"`
+	Body     string    `json:"body" binding:"required"`
+	User     Users     `gorm:"foreignKey:ID" json:"user" binding:"required"`
+	Question Questions `gorm:"foreignKey:ID" json:"question" binding:"required"`
 }
 
 type CommentsOnAnswer struct {
 	gorm.Model
 	Body   string  `json:"body"`
-	User   Users   `gorm:"foreignKey:ID" json:"user"`
-	Answer Answers `gorm:"foreignKey:ID" json:"question"`
+	User   Users   `gorm:"foreignKey:ID" json:"user" binding:"required"`
+	Answer Answers `gorm:"foreignKey:ID" json:"question" binding:"required"`
 }
 
 type Tags struct {
 	gorm.Model
-	Name string `gorm:"unique" json:"name"`
+	Name string `gorm:"unique" json:"name" binding:"required"`
 }
